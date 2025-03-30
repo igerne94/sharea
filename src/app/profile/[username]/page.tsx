68ec3b3717b20524c72072056ac232b651f7ddec
igerne94 <igerne@gmail.com>
2025-03-30 20:46:48 +0200

    profile page metadata generation

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { getProfileByName, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/profile.action"
+import { notFound } from "next/navigation"
 
-function ProfilePage({ params }: { params: { username: string } }) {
-    console.log(params.username)
-    return (
-        <div>page</div>
-  )
+export async function generateMetadata({ params }: { params: { username: string } }) { 
+    const user = await getProfileByName(params.username)
+    if (!user) return 
+    
+    return {
+        title: user.name ?? user.username,
+        description: user.bio ?? `Check out ${user.username}'s profile`,
+    }
 }
 
+async function ProfilePage({ params }: { params: { username: string } }) {
+    const user = await getProfileByName(params.username)
+
+    if (!user) notFound();
+
+    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+        getUserPosts(user.id),
+        getUserLikedPosts(user.id),
+        isFollowing(user.id),
+    ]);
+    
+    return <ProfilePageClient />
+}
 export default ProfilePage
